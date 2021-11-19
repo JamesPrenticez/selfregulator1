91c2a5b2cc57b24e12fddc85fe2a2e6cea18afb4
James Prentice <jamesprenticez@gmail.com>
2021-11-19 20:33:22 +1300

    Redux to handle hamburger

--- components/Hamburger.jsx
+++ components/Hamburger.jsx
@@ -4,19 +4,19 @@ import { toggleHamburger } from "../redux/actions/hambuger"
 
 class Hamburger extends React.Component{
   render(){
-  console.log(this.props)
+  const {hamburger} = this.props
     return (
       <div>
         <button className="text-white md:hidden ml-auto outline-none mr-8" 
-          onClick={() => toggleHamburger(true)}>
+          onClick={() => this.props.dispatch(toggleHamburger(!hamburger))}>
           <div className="flex justify-center items-center h-20 w-20 ">
               {/* --------- top ---------*/}
-              <span className={`${true ? 'absolute h-1 w-7 bg-custom-secondaryAccent rounded transform transition duration-500 ease-in-out opacity-0' : 'absolute mb-4 h-1 w-7 rounded bg-custom-secondaryAccent transform transition duration-500 ease-in-out'}`}></span>
+              <span className={`${hamburger ? 'absolute h-1 w-7 bg-custom-secondaryAccent rounded transform transition duration-500 ease-in-out opacity-0' : 'absolute mb-4 h-1 w-7 rounded bg-custom-secondaryAccent transform transition duration-500 ease-in-out'}`}></span>
               {/* --------- two in the middle ---------*/}
-              <span className={`${true ? 'absolute h-1 w-7 bg-custom-secondaryAccent rounded transform transition duration-500 ease-in-out -rotate-45' : 'absolute h-1 w-7 rounded bg-custom-secondaryAccent transform transition duration-500 ease-in-out'}`}></span>
-              <span className={`${true ? 'absolute h-1 w-7 bg-custom-secondaryAccent rounded transform transition duration-500 ease-in-out rotate-45' : 'absolute h-1 w-7 rounded bg-custom-secondaryAccent transform transition duration-500 ease-in-out '}`}></span>
+              <span className={`${hamburger ? 'absolute h-1 w-7 bg-custom-secondaryAccent rounded transform transition duration-500 ease-in-out -rotate-45' : 'absolute h-1 w-7 rounded bg-custom-secondaryAccent transform transition duration-500 ease-in-out'}`}></span>
+              <span className={`${hamburger ? 'absolute h-1 w-7 bg-custom-secondaryAccent rounded transform transition duration-500 ease-in-out rotate-45' : 'absolute h-1 w-7 rounded bg-custom-secondaryAccent transform transition duration-500 ease-in-out '}`}></span>
               {/* --------- bottom ---------*/}
-              <span className={`${true ? 'absolute h-1 w-7 bg-custom-secondaryAccentrounded transform transition duration-1000 ease-in-out opacity-0' : 'absolute mt-4 h-1 w-7 rounded bg-custom-secondaryAccent transform transition duration-500 ease-in-out'}`}></span>
+              <span className={`${hamburger ? 'absolute h-1 w-7 bg-custom-secondaryAccentrounded transform transition duration-1000 ease-in-out opacity-0' : 'absolute mt-4 h-1 w-7 rounded bg-custom-secondaryAccent transform transition duration-500 ease-in-out'}`}></span>
           </div>
         </button>
       </div>
@@ -30,10 +30,4 @@ const mapStateToProps = (state) => {
   }
 }
 
-const mapDispatchToProps = (dispatch) => {
-  return {
-    toggleHamburger: () => dispatch(toggleHamburger())
-  }
-}
-
-export default connect(mapStateToProps, mapDispatchToProps)(Hamburger)
+export default connect(mapStateToProps)(Hamburger)
--- redux/actions/hambuger.js
+++ redux/actions/hambuger.js
@@ -1,5 +1,15 @@
+// An action is a plain JavaScript object that has a type field
+// You can think of an action as an event that describes something that happened in the application.
+// The type field should be a string that gives this action a descriptive name, like "todos/todoAdded"
+// An action object can have other fields with additional information about what happened. By convention, we put that information in a field called payload.
+// An action creator is a function that creates and returns an action object. We typically use these so we don't have to write the action object by hand every time:
+export const TOGGLE_HAMBURGER = 'TOGGLE_HAMBURGER'
+
 export function toggleHamburger(bool){
-  return {
-    type: "TOOGLE_HAMBURGER", bool
+  console.log("action")
+  return ({
+    type: TOGGLE_HAMBURGER,
+    bool
   }
+  )
 }
